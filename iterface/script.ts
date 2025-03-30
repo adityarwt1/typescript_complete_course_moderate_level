f5d352d6f5f5809f13c27d4011080e41e2b03958
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-30 14:21:34 +0530

    first commit

---
+++ iterface/script.ts
@@ -0,0 +1,10 @@
+interface   User2 {
+    email:string,
+    userid: number
+}
+
+const user2:User2 = {
+    email: "a@gmail.com",
+    userid: 123645
+}
+
---
+++ tuples/script.ts
@@ -0,0 +1,4 @@
+// const user: (string | number)[] = [1,"ar"]
+const user: [string, number, boolean] = ["aditya", 50, true]
+
+let rgb:[number, number, number] = [255,255,255]
